fe862c01e08bfb2e31711a6c7231be72c9fdb94c
Francisco Moreno <francisco.moreno@sngular.com>
2020-06-22 19:27:44 +0200

    resize

--- src/main/java/SeleniumInit.java
+++ src/main/java/SeleniumInit.java
@@ -1,15 +1,18 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumInit {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+
 
         WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new FirefoxDriver();
+
+        //driver.manage().window().setSize(new Dimension(300,300));
 
         driver.get("https://duckduckgo.com/");
 
