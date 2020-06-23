e08e7c113bca7d110604d974a93f6712f196e7b5
Francisco Moreno <francisco.moreno@sngular.com>
2020-06-23 18:45:05 +0200

    login test

--- src/test/java/SeleniumTests/LoginTest.java
+++ src/test/java/SeleniumTests/LoginTest.java
@@ -3,22 +3,40 @@ package SeleniumTests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class LoginTest {
-    @Test
-    public void credencialesIncorrectas_MensajeUsuarioIvalido(){
+    WebDriver driver;
+    String URL = "https://the-internet.herokuapp.com/login";
+
+    @BeforeClass
+    public static void setUpClass() {
         System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get(URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void credencialesIncorrectas_MensajeUsuarioIvalido() {
 
         String usuarioIncorrecto = "fakeUser";
-        String passwordIncorrecto= "fakePass";
+        String passwordIncorrecto = "fakePass";
         String mensajeEsperado = "Your username is invalid!";
-
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/login");
 
         driver.findElement(By.id("username")).sendKeys(usuarioIncorrecto);
         driver.findElement(By.id("password")).sendKeys(passwordIncorrecto);
@@ -31,7 +49,26 @@ public class LoginTest {
 
         assertTrue(mensaje.startsWith(mensajeEsperado));
 
-        driver.close();
+    }
+
+    @Test
+    public void credencialesCorrectas_AccesoZonaSegura() {
+
+        String usuarioCorrecto = "tomsmith";
+        String passwordCorrecto = "SuperSecretPassword!";
+        String mensajeEsperado = "You logged into a secure area!";
+
+
+        driver.findElement(By.id("username")).sendKeys(usuarioCorrecto);
+        driver.findElement(By.id("password")).sendKeys(passwordCorrecto);
+
+        driver.findElement(By.cssSelector("#login > button")).click();
+
+        String mensaje = driver.findElement(By.id("flash")).getText();
+
+        assertTrue(driver.getCurrentUrl().endsWith("secure"));
+
+        assertTrue(mensaje.startsWith(mensajeEsperado));
 
     }
 }
