c1db779871e67319c7da5b6bf007d81772dd5369
Francisco Moreno <francisco.moreno@sngular.com>
2020-06-24 19:55:22 +0200

    cucumber

--- src/test/java/TodoMVCTest/ItemsSteps.java
+++ src/test/java/TodoMVCTest/ItemsSteps.java
@@ -1,25 +1,38 @@
 package TodoMVCTest;
 
+import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.TodoMVC.MainReactPage;
+
+import static org.testng.Assert.assertEquals;
 
 public class ItemsSteps {
     WebDriver driver;
-
+    MainReactPage mainReactPage;
     @Before
     public void setUp(){
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 
+        driver = new ChromeDriver();
+        mainReactPage = new MainReactPage(driver);
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
     }
 
     @Given("^accedo a la lista$")
     public void accedoALaLista() {
+        mainReactPage.navigateTo();
     }
 
     @Then("^la lista de elementos esta vacía$")
     public void laListaDeElementosEstaVacía() {
+        assertEquals(0,mainReactPage.getNumberOfElements(), "La lista no está vacía");
     }
 }
